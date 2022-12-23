ec74d35d13b8ea045c9d84a54c11ac4430706055
jack41402 <lucky368368@gmail.com>
2022-12-23 10:00:40 +0800

    Update main.cpp

--- TEST/TEST/main.cpp
+++ TEST/TEST/main.cpp
@@ -1,73 +1,29 @@
-#include <iostream>
+#include <stdio.h>
 
-#pragma GCC optimize("Ofast,unroll-loops")
-#pragma GCC target("sse,sse2,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-#pragma GCC optimize("inline")
-#pragma GCC optimize("-fgcse")
-#pragma GCC optimize("-fgcse-lm")
-#pragma GCC optimize("-fipa-sra")
-#pragma GCC optimize("-ftree-pre")
-#pragma GCC optimize("-ftree-vrp")
-#pragma GCC optimize("-fpeephole2")
-#pragma GCC optimize("-ffast-math")
-#pragma GCC optimize("-fsched-spec")
-#pragma GCC optimize("-falign-jumps")
-#pragma GCC optimize("-falign-loops")
-#pragma GCC optimize("-falign-labels")
-#pragma GCC optimize("-fdevirtualize")
-#pragma GCC optimize("-fcaller-saves")
-#pragma GCC optimize("-fcrossjumping")
-#pragma GCC optimize("-fthread-jumps")
-#pragma GCC optimize("-funroll-loops")
-#pragma GCC optimize("-fwhole-program")
-#pragma GCC optimize("-freorder-blocks")
-#pragma GCC optimize("-fschedule-insns")
-#pragma GCC optimize("inline-functions")
-#pragma GCC optimize("-ftree-tail-merge")
-#pragma GCC optimize("-fschedule-insns2")
-#pragma GCC optimize("-fstrict-aliasing")
-#pragma GCC optimize("-fstrict-overflow")
-#pragma GCC optimize("-falign-functions")
-#pragma GCC optimize("-fcse-skip-blocks")
-#pragma GCC optimize("-fcse-follow-jumps")
-#pragma GCC optimize("-fsched-interblock")
-#pragma GCC optimize("-fpartial-inlining")
-#pragma GCC optimize("no-stack-protector")
-#pragma GCC optimize("-freorder-functions")
-#pragma GCC optimize("-findirect-inlining")
-#pragma GCC optimize("-fhoist-adjacent-loads")
-#pragma GCC optimize("-frerun-cse-after-loop")
-#pragma GCC optimize("inline-small-functions")
-#pragma GCC optimize("-finline-small-functions")
-#pragma GCC optimize("-ftree-switch-conversion")
-#pragma GCC optimize("-foptimize-sibling-calls")
-#pragma GCC optimize("-fexpensive-optimizations")
-#pragma GCC optimize("-funsafe-loop-optimizations")
-#pragma GCC optimize("inline-functions-called-once")
-#pragma GCC optimize("-fdelete-null-pointer-checks")
+int arr[100]={0} ;
 
-using namespace std;
-
-int main ()
+int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int x , now , str_size , zero ;
-    string str ;
-    char first , temp ;
-    while (cin>>str)
+    arr[0] = 1 ;
+    for (int i=2 ; i<=50 ; ++i)
     {
-        str_size = str.size() , x = 0 , zero = -1 , first = str[0] ;
-        for (now=1 ; now<str_size ; ++now)
+        for (int j=0 ; j<100 ; ++j)
         {
-            temp = str[now] ;
-            if (temp==str[x]) zero = now - x ,  ++x ;
-            else if (temp==first) x = 1 , zero = now ;
-            else zero = -1 , x = 0 ;
+            arr[j] *= i ;
         }
-        if (str_size%zero==0) cout << str.substr (0 , zero) << '\n' ;
-        else cout << str << '\n' ;
-        cout << flush ;
+        int temp=0 ;
+        for (int j=0 ; j<100 ; ++j)
+        {
+            arr[j] += temp ;
+            temp = arr[j]/10 ;
+            arr[j] %= 10 ;
+        }
     }
-    return 0;
+    int flag=1 ;
+    for (int i=99 ; i>=0 ; --i)
+    {
+        if (arr[i]==0 && flag) continue ;
+        else flag=0 ;
+        printf ("%d" , arr[i]) ;
+    }
 }
