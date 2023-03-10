0fe8f92e74034384e7c1708575616961cd00f9e1
jack41402 <lucky368368@gmail.com>
2023-03-10 12:00:54 +0800

    Sliding Median undone

--- CSES/Sliding Median/main.cpp
+++ CSES/Sliding Median/main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 multiset<long long int> upper , lower ;
+long long int arr[200005] ;
 long long int n , k , temp ;
 
 void insert (long long int num)
@@ -21,7 +22,7 @@ void insert (long long int num)
     }
     else
     {
-        lower.emplace(temp) ;
+        lower.emplace(num) ;
         if (lower.size()>(k+1)/2)
         {
             temp = *lower.rbegin() ;
@@ -33,7 +34,14 @@ void insert (long long int num)
 
 void erase (long long int num)
 {
-
+    if (upper.find(num)!=upper.end()) upper.erase(upper.find(num)) ;
+    else lower.erase(lower.find(num));
+    if (lower.empty())
+    {
+        temp = *upper.begin() ;
+        lower.emplace(temp) ;
+        upper.erase(upper.begin()) ;
+    }
 }
 
 int main ()
@@ -41,12 +49,24 @@ int main ()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> n >> k ;
-    cin >> temp ;
-    lower.emplace(temp) ;
-    for (int i=1 ; i<n ; ++i)
+    for (int i=0 ; i<n ; ++i) cin >> arr[i] ;
+    lower.emplace(arr[0]) ;
+    for (int i=1 ; i<k ; ++i) insert(arr[i]) ;
+    cout << *lower.rbegin() << ' ' ;
+    for (int i=k ; i<n ; ++i)
     {
-        cin >> temp ;
-        insert(temp) ;
+        if (k==1)
+        {
+            insert(arr[i]) ;
+            erase(arr[i-k]) ;
+        }
+        else
+        {
+            erase(arr[i-k]) ;
+            insert(arr[i]) ;
+        }
+        cout << *lower.rbegin() << ' ' ;
     }
+    cout << '\n' ;
     return 0;
 }
