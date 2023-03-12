583fc7e8b40723759b64306d762aebdc621b37c7
jack41402 <lucky368368@gmail.com>
2023-03-13 02:01:33 +0800

    ZeroJudge d424 done

--- ZeroJudge/d424/main.cpp
+++ ZeroJudge/d424/main.cpp
@@ -10,7 +10,7 @@ int main ()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int l , r , h , height ;
-    vector<pair<long long int , long long int>> arr ;
+    vector<pair<long long int , long long int>> arr , ans ;
     multiset<long long int , greater<>> s ;
     while (cin>>l>>h>>r)
     {
@@ -23,10 +23,32 @@ int main ()
     sort(arr.begin() , arr.end()) ;
     // 建築物輪廓的最後必定為 0
     s.emplace(0) ;
-    height = 0 ;
     for (auto i : arr)
     {
-
+        // 更新當前最高的高度
+        if (i.second>=0) s.emplace(i.second) ;
+        // 遍歷完該建築物後，刪除該建築物的高度
+        else s.erase(s.find(-i.second)) ;
+        // 符合條件時，向 ans 加入座標以及當前最高的高度
+        if (ans.empty() || i.first!=ans.back().first)
+        {
+            ans.emplace_back(i.first , *s.begin()) ;
+        }
+        // 如果座標相同時，更新該座標的高度
+        else
+        {
+            ans[ans.size()-1] = {i.first , *s.begin()} ;
+        }
     }
+    height = 0 ;
+    for (auto i : ans)
+    {
+        if (i.second!=height)
+        {
+            cout << i.first << ' ' << i.second << ' ' ;
+        }
+        height = i.second ;
+    }
+    cout << '\n' ;
     return 0;
 }
