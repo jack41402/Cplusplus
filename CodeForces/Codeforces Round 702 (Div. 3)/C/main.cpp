e72507deb2fc2569b17ba5339fbd8b4814bb37aa
jack41402 <lucky368368@gmail.com>
2023-03-14 08:56:22 +0800

    Update main.cpp

--- CodeForces/Codeforces Round 702 (Div. 3)/C/main.cpp
+++ CodeForces/Codeforces Round 702 (Div. 3)/C/main.cpp
@@ -8,21 +8,22 @@ int main ()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     long long int t , x , p ;
-    bool flag=false ;
     cin >> t ;
     while (t--)
     {
         cin >> x ;
-        for (long long int a=1 ; a*a*a<=x ; ++a)
+        bool flag=false ;
+        for (long long int a=1 ; a*a*a<x ; ++a)
         {
-            p = (long long int) cbrt(x-a*a*a) ;
+            p = cbrt(x-a*a*a) ;
             if (p*p*p==x-a*a*a)
             {
                 flag = true ;
                 break ;
             }
         }
+        if (flag) cout << "Yes\n" ;
+        else cout << "No\n" ;
     }
-    if (flag) cout <<
     return 0;
 }
