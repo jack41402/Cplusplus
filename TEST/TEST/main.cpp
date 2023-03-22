394f18471a22c4064dcb16ccdc66c4625e931a61
jack41402 <lucky368368@gmail.com>
2023-03-23 04:42:26 +0800

    Update main.cpp

--- TEST/TEST/main.cpp
+++ TEST/TEST/main.cpp
@@ -1,29 +1,25 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
 
-int arr[100]={0} ;
+using namespace std;
+
+int n , m ;
+vector<int> pour ;
 
 int main()
 {
-    arr[0] = 1 ;
-    for (int i=2 ; i<=50 ; ++i)
+    for (int i=0 ; i<3 ; ++i)
     {
-        for (int j=0 ; j<100 ; ++j)
-        {
-            arr[j] *= i ;
-        }
-        int temp=0 ;
-        for (int j=0 ; j<100 ; ++j)
-        {
-            arr[j] += temp ;
-            temp = arr[j]/10 ;
-            arr[j] %= 10 ;
-        }
+        cin >> m ;
+        pour.emplace_back(m) ;
     }
-    int flag=1 ;
-    for (int i=99 ; i>=0 ; --i)
+    for (auto i : pour)
     {
-        if (arr[i]==0 && flag) continue ;
-        else flag=0 ;
-        printf ("%d" , arr[i]) ;
+        pour.emplace_back(-i) ;
     }
+    for (auto i : pour)
+    {
+        cout << i << endl ;
+    }
+    return 0;
 }
