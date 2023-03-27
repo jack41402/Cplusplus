59efe4c517c805a603f07e4daf54f2ad0bb1c474
jack41402 <lucky368368@gmail.com>
2023-03-27 09:35:26 +0800

    TIOJ 1008 done

--- TIOJ/1008/main.cpp
+++ TIOJ/1008/main.cpp
@@ -3,63 +3,68 @@
 #include <numeric>
 #include <vector>
 #include <queue>
+#include <set>
 
 using namespace std;
 
-int n , m , t , ans=1e9  , temp ;
-vector<int> pour , water ;
-queue<pair<vector<int> , int>> q ;
+int n , m , t , ans=1e9 ;
+int to_int[130] ;
+char temp , to_c[63]={"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"} ;
+string water ;
+vector<int> pour ;
+queue<pair<string , int>> q ;
+set<string> s ;
 
 void bfs ()
 {
     while (!q.empty())
     {
-        water.clear() ;
-        for (auto i : q.front().first) water.emplace_back(i) ;
+        water = q.front().first ;
         ans = q.front().second ;
+        s.emplace(water) ;
         q.pop() ;
-        if (find(water.begin() , water.end() , t)!=water.end()) break ;
+        if (find(water.begin() , water.end() , to_c[t])!=water.end()) break ;
         for (int i=0 ; i<n ; ++i)
         {
             // 把燒杯裝滿水
-            if (water[i]<pour[i])
+            if (to_int[water[i]]<pour[i])
             {
                 temp = water[i] ;
-                water[i] = pour[i] ;
-                q.emplace(water , ans+1) ;
+                water[i] = to_c[pour[i]] ;
+                if (s.find(water)==s.end()) q.emplace(water , ans+1) ;
                 water[i] = temp ;
             }
             // 把水倒掉
-            if (water[i]!=0)
+            if (water[i]!='0')
             {
                 temp = water[i] ;
-                water[i] = 0 ;
-                q.emplace(water , ans+1) ;
+                water[i] = '0' ;
+                if (s.find(water)==s.end()) q.emplace(water , ans+1) ;
                 water[i] = temp ;
                 for (int j=0 ; j<n ; ++j)
                 {
                     if (j==i) continue ;
-                    if (water[j]!=pour[j])
+                    if (to_int[water[j]]!=pour[j])
                     {
                         // 要倒入水的燒杯容量比現有的水量大，把水全倒到燒杯
-                        if (pour[j] - water[j]>water[i])
+                        if (pour[j] - to_int[water[j]]>to_int[water[i]])
                         {
                             temp = water[i] ;
-                            water[j] += water[i] ;
-                            water[i] = 0 ;
-                            q.emplace(water , ans+1) ;
+                            water[j] = to_c[to_int[water[j]] + to_int[water[i]]] ;
+                            water[i] = '0' ;
+                            if (s.find(water)==s.end()) q.emplace(water , ans+1) ;
                             water[i] = temp ;
-                            water[j] -= water[i] ;
+                            water[j] = to_c[to_int[water[j]] - to_int[water[i]]] ;
                         }
                         // 要倒入水的燒杯容量比現有的水量小，將燒杯倒滿
                         else
                         {
                             temp = water[j] ;
-                            water[i] -=pour[j] - water[j] ;
-                            water[j] = pour[j] ;
-                            q.emplace(water , ans+1) ;
+                            water[i] = to_c[to_int[water[i]] - (pour[j] - to_int[water[j]])] ;
+                            water[j] = to_c[pour[j]] ;
+                            if (s.find(water)==s.end()) q.emplace(water , ans+1) ;
                             water[j] = temp ;
-                            water[i] +=pour[j] - water[j] ;
+                            water[i] = to_c[to_int[water[i]] + (pour[j] - to_int[water[j]])] ;
                         }
                     }
                 }
@@ -95,12 +100,17 @@ int main ()
         cout << -1 << '\n' ;
         return 0;
     }
-    vector<int> arr(n) ;
-    q.emplace(arr , 0) ;
+    for (int i=0 ; i<62 ; ++i)
+    {
+        to_int[to_c[i]] = i ;
+    }
+    for (int i=0 ; i<n ; ++i) water += '0' ;
+    q.emplace(water , 0) ;
     bfs() ;
     while (!q.empty())
     {
-        if (find(q.front().first.begin() , q.front().first.end() , t)!=q.front().first.end())
+        auto pos=find(q.front().first.begin() , q.front().first.end() , to_c[t]) ;
+        if (pos!=q.front().first.end())
         {
             ans = min(ans , q.front().second) ;
         }
