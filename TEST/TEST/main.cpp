d3097918eb9a7e8e1a632994a27fd528fd6cf8ab
jack41402 <lucky368368@gmail.com>
2023-03-27 09:35:57 +0800

    Update TEST.cpp

--- TEST/TEST/main.cpp
+++ TEST/TEST/main.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
+#include <algorithm>
+#include <numeric>
 #include <vector>
+#include <queue>
+#include <set>
 
 using namespace std;
 
-int n , m ;
-vector<int> pour ;
+int n , m , t , ans=1e9 ;
+int to_int[130] ;
+char temp , to_c[63] ;
 
-int main()
+
+int main ()
 {
-    for (int i=0 ; i<3 ; ++i)
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n ;
+    for (int i=0 ; i<10 ; ++i)
     {
-        cin >> m ;
-        pour.emplace_back(m) ;
+        to_int['0'+i] = i ;
+        to_c[i] = '0'+i ;
+        cout << char('0'+i) << int('0'+i) << endl ;
     }
-    for (auto i : pour)
+    for (int i=0 ; i<26 ; ++i)
     {
-        pour.emplace_back(-i) ;
+        to_int['A'+i] = 10+i ;
+        to_c[10+i] = 'A'+i ;
+        cout << char('A'+i) << int('A'+i) << endl ;
     }
-    for (auto i : pour)
+    for (int i=0 ; i<26 ; ++i)
     {
-        cout << i << endl ;
+        to_int['a'+i] = 36 + i ;
+        to_c[36+i] = 'a'+i ;
+        cout << char('a'+i) << int('a'+i) << endl ;
     }
     return 0;
 }
