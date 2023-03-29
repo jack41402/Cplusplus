804f776d548e3e241e9e3b7b19492636df783c1b
jack41402 <lucky368368@gmail.com>
2023-03-29 08:30:32 +0800

    Update Educational TPR#1 I

--- CodeForces/Educational Ten Point Round #1 Sorting and Searching I (Div. 2)/I/main.cpp
+++ CodeForces/Educational Ten Point Round #1 Sorting and Searching I (Div. 2)/I/main.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
 
 using namespace std;
 
-int arr[10] , b[200005] ;
+int arr[10] ;
 
 int main ()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n ;
+    long long int n , temp , ans=1e18 , l , r ;
     for (int &i : arr) cin >> i ;
     cin >> n ;
+    vector<pair<long long int , long long int>> b ;
     for (int i=0 ; i<n ; ++i)
     {
-        cin >> b[i] ;
+        cin >> temp ;
+        b.emplace_back(temp , i) ;
+    }
+    sort(b.begin() , b.end()) ;
+    l=0 , r=0 ;
+    set<long long int> s ;
+    vector<long long int> c(n) ;
+    c[b[0].second]++ ;
+    s.emplace(b[0].second) ;
+    while (l<b.size())
+    {
+        while (s.size()<n && r<b.size())
+        {
+            r++ ;
+            if (r==b.size()) break ;
+            c[b[r].second]++ ;
+            s.emplace(b[r].second) ;
+        }
+        if (s.size()==n)
+        {
+            int
+        }
     }
     return 0;
 }
