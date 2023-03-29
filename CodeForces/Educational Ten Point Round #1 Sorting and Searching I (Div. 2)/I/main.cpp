1b041a24bcb7ac00520bf7b5e4a2d5afae35bdc2
jack41402 <lucky368368@gmail.com>
2023-03-29 22:51:05 +0800

    Update Educational TPR#1 I

--- CodeForces/Educational Ten Point Round #1 Sorting and Searching I (Div. 2)/I/main.cpp
+++ CodeForces/Educational Ten Point Round #1 Sorting and Searching I (Div. 2)/I/main.cpp
@@ -5,21 +5,26 @@
 
 using namespace std;
 
-int arr[10] ;
+int a[10] ;
 
 int main ()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     long long int n , temp , ans=1e18 , l , r ;
-    for (int &i : arr) cin >> i ;
+    for (int &i : a) cin>>i ;
     cin >> n ;
     vector<pair<long long int , long long int>> b ;
     for (int i=0 ; i<n ; ++i)
     {
         cin >> temp ;
-        b.emplace_back(temp , i) ;
+        // 枚舉在 a 中所有 <=temp 的值
+        for (int j : a)
+        {
+            if (j<=temp) b.emplace_back(temp-j , i) ;
+        }
     }
+    // index=0 可以視為最小值，index=b.size()-1 可以視為最大值
     sort(b.begin() , b.end()) ;
     l=0 , r=0 ;
     set<long long int> s ;
@@ -28,6 +33,7 @@ int main ()
     s.emplace(b[0].second) ;
     while (l<b.size())
     {
+        // r<b.size() can be ignored
         while (s.size()<n && r<b.size())
         {
             r++ ;
@@ -37,8 +43,17 @@ int main ()
         }
         if (s.size()==n)
         {
-            int
+            long long int diff=b[r].first - b[l].first ;
+            ans = min(ans , diff) ;
+            if (c[b[l].second]==1)
+            {
+                s.erase(b[l].second) ;
+            }
+            c[b[l].second]-- ;
+            l++ ;
         }
+        else break ;
     }
+    cout << ans << '\n' ;
     return 0;
 }
